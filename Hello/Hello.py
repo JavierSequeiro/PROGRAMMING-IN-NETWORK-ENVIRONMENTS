445b93828a3e7eb183daad786b44b5b45c1879f6
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-17 10:22:42 +0100

    FIBONACCI EXERCISE DONE

--- Hello/Hello.py
+++ Hello/Hello.py
@@ -1,1 +1,15 @@
-print("Javier")
+addition = 0
+for n in range (1, 21):
+    addition += n
+
+print(addition)
+
+def sum(n):
+    summatory = 0
+    for i in range (1, n + 1):
+        summatory += i
+    return summatory
+
+#number = int(input("Enter the last element of the sum: "))
+print(f"The sum of the first {20} numbers  is ", sum(20))
+
