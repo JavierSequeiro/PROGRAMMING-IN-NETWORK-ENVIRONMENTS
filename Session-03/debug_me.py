b3ef40b52ba39da2698d9c6455b26859c4eea49e
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-17 18:38:47 +0100

    PROGRAM DEBUGGED CORRECTLY

--- Session-03/debug_me.py
+++ Session-03/debug_me.py
@@ -5,8 +5,11 @@ def g(a, b):
 def f(a, b, c, d):
     t0 = a + b - g(a, 0)
     t1 = g(c, d)
-    t3 = 2 * (t0 / t1)
-    return t0 + 2*t1 + t3*t3
+    if c != d:
+        t3 = 2 * (t0 / t1)
+        return t0 + 2*t1 + t3*t3
+    else:
+        return "You must input a c value different from d."
 
 
 # -- Main program
