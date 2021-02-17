9ac8bd1a39213f839d97126ffdff6b4d8790021a
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-17 18:39:51 +0100

    2ND EXERCISE OF DNA, COUNT THEM FROM A FILE

---
+++ Session-03/dna_count_file.py
@@ -0,0 +1,34 @@
+def read_file(file):
+    nucleotides_list = []
+    with open(file, "r") as f:
+        for line in f:
+            line = line.strip("\n")
+            for c in line:
+                nucleotides_list.append(c)
+    return nucleotides_list
+
+def count_nucletides(nuc_list):
+    a = 0
+    c = 0
+    t = 0
+    g = 0
+    for i in nuc_list:
+        if i == "A":
+            a += 1
+        elif i == "C":
+            c += 1
+        elif i == "T":
+            t += 1
+        else:
+            g += 1
+    return a, c, t, g
+
+file = "dna.txt"
+a, c, t, g = count_nucletides(read_file(file))
+
+print(f"A: {a} \n"
+      f"C: {c} \n"
+      f"T: {t} \n"
+      f"G: {g} \n")
+
+print(f"Number of bases: {len(set(read_file(file)))}")
