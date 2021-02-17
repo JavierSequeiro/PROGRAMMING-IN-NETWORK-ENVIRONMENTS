6033134da3f19129c67481373c382c3d731986aa
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-17 17:49:03 +0100

    SUM20 FILE

---
+++ Session-01/sum20.py
@@ -0,0 +1,8 @@
+def sum(n):
+    summatory = 0
+    for i in range (1, n + 1):
+        summatory += i
+    return summatory
+
+#number = int(input("Enter the last element of the sum: ")) =)
+print(f"The sum of the first {20} numbers  is ", sum(20))
