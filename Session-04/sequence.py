ddfdf2bbd89cc89f8b32d02b3dcef5bb96c0ef9f
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-23 23:19:51 +0100

    EXERCISE 5 (COUNT BASES)

---
+++ Session-04/sequence.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+filename = "ADA.txt"
+
+file_content = Path(filename).read_text()
+useful_genome = file_content[file_content.find("\n"):].replace("\n", "")
+print(f"There are {len(useful_genome)} bases in {filename} gene")
