0354e194b9e7714cfa3ef96accc93999a3691963
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-23 22:22:18 +0100

    EXERCISE 2

--- Session-04/head.py
+++ Session-04/head.py
@@ -3,7 +3,7 @@ from pathlib import Path
 FILENAME = input("Enter the name of the file containing the gene info: ")
 
 genome = Path(FILENAME).read_text()
-gene_info = ""
+gene_info = f"First line of the {FILENAME} file: \n"
 for c in genome:
     if c == "\n":
         print(gene_info)
