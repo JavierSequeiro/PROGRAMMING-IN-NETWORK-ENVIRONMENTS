e57939fe02137b5064c86ff88b4c060b3dcb7e9e
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-24 18:22:09 +0100

    EXERCISE 3 LENGTH OF GENES

---
+++ P0/Ex3.py
@@ -0,0 +1,11 @@
+import Seq0
+
+U5 = "../Session-04/U5.txt"
+ADA = "../Session-04/ADA.txt"
+FRAT1 = "../Session-04/FRAT1.txt"
+FXN = "../Session-04/FXN.txt"
+print("-----| Exercise 3 |------")
+print(f"Gene U5 ---> Length: {Seq0.seq_len(U5)} \n"
+      f"Gene ADA ---> Length: {Seq0.seq_len(ADA)} \n"
+      f"Gene FRAT1 ---> Length: {Seq0.seq_len(FRAT1)} \n"
+      f"Gene FXN ---> Length: {Seq0.seq_len(FXN)} \n")
