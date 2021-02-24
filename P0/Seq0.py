cbce8c8fbef3ad1b52e51fae7da00dd1ab4fa9ee
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-24 18:29:45 +0100

    MODULE WITH DNA FUNCTIONS

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -13,3 +13,9 @@ def seq_len(filename):
     file_contents = Path(filename).read_text()
     useful_genome = file_contents[file_contents.find("\n"):].replace("\n","")
     return len(useful_genome)
+
+def seq_count_bases(filename):
+    file_contents = Path(filename).read_text()
+    useful_genome = file_contents[file_contents.find("\n"):].replace("\n", "")
+    return useful_genome.count("A"), useful_genome.count("C"), useful_genome.count("T"), useful_genome.count("G")
+
