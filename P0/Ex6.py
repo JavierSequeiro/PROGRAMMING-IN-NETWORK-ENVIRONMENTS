caacdd149ee3818ba114b4549c9c064ca3fbd59a
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-02-25 00:21:17 +0100

    EXERCISE 6 GET REVERSE SEQUENCE

---
+++ P0/Ex6.py
@@ -0,0 +1,10 @@
+import Seq0
+
+folder = "../Session-04/"
+gene = "U5"
+sequence = Seq0.seq_read_fasta(folder + gene + ".txt")[0:20]
+print("-----| Exercise 5 |------ \n")
+print(f"Gene {gene}")
+print(f"Frag: {sequence}")
+print(f"Rev : {Seq0.seq_reverse(sequence)}")
+
