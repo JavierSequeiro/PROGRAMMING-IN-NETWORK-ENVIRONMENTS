be5ff6acdc5cdc647115cfcba71a1fac070ce702
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-02 16:00:39 +0100

    ADDED NEW FUNCTIONS USEFUL FOR SEQUENCES

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -14,10 +14,29 @@ def seq_len(filename):
     useful_genome = file_contents[file_contents.find("\n"):].replace("\n","")
     return len(useful_genome)
 
+def seq_len_sequence(sequence):
+    return len(sequence)
+
 def seq_count_bases(filename):
     file_contents = Path(filename).read_text()
     useful_genome = file_contents[file_contents.find("\n"):].replace("\n", "")
     return useful_genome.count("A"), useful_genome.count("C"), useful_genome.count("T"), useful_genome.count("G")
+
+def seq_count_bases_sequence(dna):
+    a = 0
+    c = 0
+    t = 0
+    g = 0
+    for i in dna:
+        if i == "A":
+            a +=1
+        elif i == "C":
+            c += 1
+        elif i == "T":
+            t += 1
+        else:
+            g += 1
+    return a, c, t, g
 
 def seq_reverse(sequence):
     return sequence[::-1]
@@ -38,3 +57,8 @@ def seq_complement(sequence):
             complement += "C"
     return complement
 
+def seq_check(sequence):
+    for nucleotide in sequence:
+        if nucleotide != "A" and nucleotide != "C" and nucleotide != "T" and nucleotide != "G":
+            return False
+    return True
