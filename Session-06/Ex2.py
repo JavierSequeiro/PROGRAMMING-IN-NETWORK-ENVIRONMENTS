7330181fd19bf2b25919f6e75d0e26d4527c9e3c
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-02 17:44:24 +0100

    EXERCISE 2: GET GENE AND ITS LENGTH FROM A LIST

---
+++ Session-06/Ex2.py
@@ -0,0 +1,36 @@
+class Seq:
+    """A class for presenting sequences"""
+    def __init__(self, strbases):
+        self.strbases = strbases
+        count = 0
+        for nucleotide in strbases:
+            if nucleotide != "A" and nucleotide != "C" and nucleotide != "T" and nucleotide != "G":
+                count += 1
+        if count != 0:
+            self.strbases = "ERROR"
+            print("ERROR")
+        #else:
+            #print("New sequence created!")
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+
+        # We just return the string with the sequence
+        return self.strbases
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        return len(self.strbases)
+
+
+seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
+
+def print_seqs(seq_list):
+    data_list = []
+    for sequence in seq_list:
+         data = f"Sequence {seq_list.index(sequence)}: (Length: {Seq.len(sequence)}) {sequence}"
+         data_list.append(data)
+    return data_list
+
+for gene in print_seqs(seq_list):
+    print(gene)
