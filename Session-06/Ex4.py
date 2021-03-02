17a0c8355edfd8b12bfe5047492aa3ea4a1440e8
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-02 22:39:06 +0100

    EXERCISE 4: PRINT PATTERN IN COLOR

---
+++ Session-06/Ex4.py
@@ -0,0 +1,45 @@
+import termcolor
+class Seq:
+    """A class for presenting sequences"""
+    def __init__(self, strbases):
+        self.strbases = strbases
+        print("New sequence created!")
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+
+        # We just return the string with the sequence
+        return self.strbases
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        return len(self.strbases)
+
+def print_seqs(seq_list):
+    data_list = []
+    for sequence in seq_list:
+         data = f"Sequence {seq_list.index(sequence)}: (Length: {Seq.len(sequence)}) {sequence}"
+         data_list.append(data)
+    return data_list
+
+def generate_seqs(pattern, number):
+    pattern_list = []
+    gene = ""
+    for c in range(0, number):
+        gene += pattern
+        pattern_list.append(Seq(gene))
+    return pattern_list
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+termcolor.cprint("List 1:", "blue")
+print_seqs(seq_list1)
+for gene in print_seqs(seq_list1):
+    termcolor.cprint(gene, "blue")
+
+
+termcolor.cprint("List 2:", "green")
+print_seqs(seq_list2)
+for gene in print_seqs(seq_list2):
+    termcolor.cprint(gene, "green")
