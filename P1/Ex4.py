0702f5554ad1365757e5dd7e9ad123264bcd2a19
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-04 00:07:16 +0100

    EXERCISE 4: CALCULATE LENGTH OF SEQUENCE(IF NULL OR INVALID LEN = 0)

---
+++ P1/Ex4.py
@@ -0,0 +1,10 @@
+from Seq1 import Seq
+
+print("-----| Practice 1, Exercise 4 |------")
+seq1 = Seq()
+seq2 = Seq("ACTGA")
+seq3 = Seq("ASSFFFJ")
+
+print(f"Sequence 1: (Length: {Seq.len(seq1)}) {seq1}")
+print(f"Sequence 2: (Length: {Seq.len(seq2)}) {seq2}")
+print(f"Sequence 3: (Length: {Seq.len(seq3)}) {seq3}")
