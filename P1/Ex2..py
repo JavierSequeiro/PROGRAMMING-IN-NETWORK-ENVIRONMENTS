a88cacd69c726944abb32835036c0ef3df82a077
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-03 18:47:42 +0100

    EXERCISE 2: CHECK IF SEQUENCE IS NULL

--- P1/Ex2..py
+++ P1/Ex2..py
@@ -4,7 +4,7 @@ print("-----| Practice 1, Exercise 2 |------")
 
 seq1 = Seq()
 seq2 = Seq("ACTG")
-seq3 = Seq("JAJAJA")
+seq3 = Seq("Invalid sequence")
 
 print(f"Sequence 1: {seq1}")
 print(f"Sequence 2: {seq2}")
