11bb37a9145921b4334a0d6ffdcacab1e48c5124
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-03 18:41:12 +0100

    EXERCISE 2: NULL, GOOD OR INVALID

---
+++ P1/Ex2..py
@@ -0,0 +1,11 @@
+from Seq1 import Seq
+
+print("-----| Practice 1, Exercise 2 |------")
+
+seq1 = Seq()
+seq2 = Seq("ACTG")
+seq3 = Seq("JAJAJA")
+
+print(f"Sequence 1: {seq1}")
+print(f"Sequence 2: {seq2}")
+print(f"Sequence 3: {seq3}")
