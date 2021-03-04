9f6dbaa82866233f80ecfc99a32295e8df56a32f
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-04 10:06:26 +0100

    EXERCISE 7: INCLUDE THE REVERSE FUNCTION ON THE PROGRAM

---
+++ P1/Ex7.py
@@ -0,0 +1,14 @@
+from Seq1 import Seq
+
+print("-----| Practice 1, Exercise 7 |-----")
+
+seq1 = Seq()
+seq2 = Seq("ACTGA")
+seq3 = Seq("Invalid sequence")
+sequence_list = [seq1, seq2, seq3]
+
+for sequence in sequence_list:
+    bases_dict = {"A": Seq.count_base(sequence)[0], "C": Seq.count_base(sequence)[1], "T": Seq.count_base(sequence)[2], "G": Seq.count_base(sequence)[3], }
+    print(f"Sequence {sequence_list.index(sequence)}: (Length: {Seq.len(sequence)}) {sequence}")
+    print(bases_dict)
+    print(f"Rev: {Seq.reverse(sequence)}")
