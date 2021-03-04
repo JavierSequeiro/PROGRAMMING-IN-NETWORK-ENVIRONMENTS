e9990e804f6164c81736ade41318851caef9934d
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-04 10:00:29 +0100

    EXERCISE 6: COUNT BASES DICTIONARY

--- P1/Ex6.py
+++ P1/Ex6.py
@@ -7,8 +7,7 @@ seq2 = Seq("ACTGA")
 seq3 = Seq("Invalid sequence")
 sequence_list = [seq1, seq2, seq3]
 
-for c in range(0, len(sequence_list)):
-    for sequence in sequence_list:
-        bases_dict = {"A": Seq.count_base(sequence)[0], "C": Seq.count_base(sequence)[1], "T": Seq.count_base(sequence)[2], "G": Seq.count_base(sequence)[3], }
-        print(f"Sequence {c}: (Length: {Seq.len(sequence)}) {sequence}")
-        print(bases_dict)
+for sequence in sequence_list:
+    bases_dict = {"A": Seq.count_base(sequence)[0], "C": Seq.count_base(sequence)[1], "T": Seq.count_base(sequence)[2], "G": Seq.count_base(sequence)[3], }
+    print(f"Sequence {sequence_list.index(sequence)}: (Length: {Seq.len(sequence)}) {sequence}")
+    print(bases_dict)
