dc7c6ea9046d2c33732ef75a4f4cef03c631d569
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-10 17:49:32 +0100

    EXERCISE 5: IMPROVED

--- P1/Ex5.py
+++ P1/Ex5.py
@@ -1,14 +1,21 @@
 from Seq1 import Seq
 
+def print_counts(i, sequence):
+    print(f"Sequence {i}: (Length: {Seq.len(sequence)}) {sequence}")
+    print( f" A: {Seq.count_base(sequence)[0]},  C: {Seq.count_base(sequence)[1]},  T: {Seq.count_base(sequence)[2]},  G: {Seq.count_base(sequence)[3]}")
+
+
 print("-----| Practice 1, Exercise 5 |------")
 
 seq1 = Seq()
 seq2 = Seq("ACTGA")
 seq3 = Seq("Invalid sequence")
+sequence_list = [seq1, seq2, seq3]
+print_counts(1, seq1)
+print_counts(2, seq2)
+print_counts(3, seq3)
 
-print(f"Sequence 1: (Length: {Seq.len(seq1)}) {seq1}")
-print(f" A: {Seq.count_base(seq1)[0]},  C: {Seq.count_base(seq1)[1]},  T: {Seq.count_base(seq1)[2]},  G: {Seq.count_base(seq1)[3]}")
-print(f"Sequence 2: (Length: {Seq.len(seq2)}) {seq2}")
-print(f" A: {Seq.count_base(seq2)[0]},  C: {Seq.count_base(seq2)[1]},  T: {Seq.count_base(seq2)[2]},  G: {Seq.count_base(seq2)[3]}")
-print(f"Sequence 3: (Length: {Seq.len(seq3)}) {seq3}")
-print(f" A: {Seq.count_base(seq3)[0]},  C: {Seq.count_base(seq3)[1]},  T: {Seq.count_base(seq3)[2]},  G: {Seq.count_base(seq3)[3]}")
+# WITH A FOR LOOP
+for i in range(1, len(sequence_list) + 1):
+    print_counts(i, sequence_list[i - 1])
+
