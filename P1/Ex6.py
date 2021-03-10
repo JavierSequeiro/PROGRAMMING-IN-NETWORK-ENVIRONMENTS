ab7e2221793fdf584b02e97fe9392d58dd29c041
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-10 17:59:44 +0100

    EXERCISE 6: USED BASES WITH FUNCTION WITH COUNT METHOD

--- P1/Ex6.py
+++ P1/Ex6.py
@@ -8,6 +8,7 @@ seq3 = Seq("Invalid sequence")
 sequence_list = [seq1, seq2, seq3]
 
 for sequence in sequence_list:
-    bases_dict = {"A": Seq.count_base(sequence)[0], "C": Seq.count_base(sequence)[1], "T": Seq.count_base(sequence)[2], "G": Seq.count_base(sequence)[3], }
+    a, c, t, g = Seq.count_base(sequence)
+    bases_dict = {"A": a, "C": c, "T": t, "G": g, }
     print(f"Sequence {sequence_list.index(sequence)}: (Length: {Seq.len(sequence)}) {sequence}")
     print(bases_dict)
