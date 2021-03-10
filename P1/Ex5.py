978883f1353c07e46eb7f51ff24583d6817d701a
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-10 17:59:23 +0100

    EXERCISE 5: USED BASES WITH NO COUNT METHOD

--- P1/Ex5.py
+++ P1/Ex5.py
@@ -2,7 +2,8 @@ from Seq1 import Seq
 
 def print_counts(i, sequence):
     print(f"Sequence {i}: (Length: {Seq.len(sequence)}) {sequence}")
-    print( f" A: {Seq.count_base(sequence)[0]},  C: {Seq.count_base(sequence)[1]},  T: {Seq.count_base(sequence)[2]},  G: {Seq.count_base(sequence)[3]}")
+    a, c, t, g = Seq.count_bases_no_count(sequence)
+    print( f" A: {a},  C: {c},  T: {t},  G: {g}")
 
 
 print("-----| Practice 1, Exercise 5 |------")
@@ -10,12 +11,14 @@ print("-----| Practice 1, Exercise 5 |------")
 seq1 = Seq()
 seq2 = Seq("ACTGA")
 seq3 = Seq("Invalid sequence")
-sequence_list = [seq1, seq2, seq3]
-print_counts(1, seq1)
-print_counts(2, seq2)
-print_counts(3, seq3)
+
+# NO LOOP
+#print_counts(1, seq1)EXERCISE
+#print_counts(2, seq2)
+#print_counts(3, seq3)
 
 # WITH A FOR LOOP
+sequence_list = [seq1, seq2, seq3]
 for i in range(1, len(sequence_list) + 1):
     print_counts(i, sequence_list[i - 1])
 
