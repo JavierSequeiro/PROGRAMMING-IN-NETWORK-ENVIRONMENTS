81af72fa36fe4ba34798d7a9bc425cde1be828e5
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-17 18:54:16 +0100

    PRACTICE 2: EXERCISE 5

---
+++ P2/Ex5.py
@@ -0,0 +1,9 @@
+from Client0 import Client
+from pathlib import Path
+
+print()
+
+s = Client("192.168.1.38", 8080)
+gene = Path("../Session-04/U5.txt").read_text()
+print(s.talk("Sending U5 gene to the server..."))
+print(s.talk(gene))
