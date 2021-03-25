2a96418093c67077ac640e69bea30531ded7cc5e
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-25 09:43:05 +0100

    PRACTICE 2: EXERCISE 6 SEND GENE FRAGMENTS

--- P2/Ex6.py
+++ P2/Ex6.py
@@ -1,17 +1,17 @@
 from Client0 import Client
 from Seq1 import Seq
 import colorama
+from colorama import Fore
 
-print("------| Practice 2, Exercise 5 |------")
+print("------| Practice 2, Exercise 6 |------")
 s = Client("192.168.1.38", 8080)
 gene_name = "FRAT1"
 gene_file = f"../Session-04/{gene_name}.txt"
 colorama.init(autoreset=True)
 useful_gene = Seq().read_fasta(gene_file)
-client, server = s.debug_talk(useful_gene)
-print(f"Gene {gene_name}: {client}")
+print(f"Gene {gene_name}: {Fore.BLUE + useful_gene}")
 
-for i in range (0, 5):
+for i in range(0, 5):
     fragment = f"Fragment {i+1}: {useful_gene[10*i:10 + 10*i]}"
     gene, response = s.debug_talk(fragment)
     print(gene)
