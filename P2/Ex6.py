20c9cd0d36881b35e423625f19b0803c54cdbb4f
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-25 08:38:11 +0100

    PRACTICE 2: EXERCISE 6 SEND GENE FRAGMENTS

---
+++ P2/Ex6.py
@@ -0,0 +1,16 @@
+from Client0 import Client
+from Seq1 import Seq
+import colorama
+
+print("------| Practice 2, Exercise 5 |------")
+s = Client("192.168.1.38", 8080)
+gene_name = "FRAT1"
+gene_file = f"../Session-04/{gene_name}.txt"
+colorama.init(autoreset=True)
+useful_gene = Seq().read_fasta(gene_file)
+client, server = s.debug_talk(useful_gene)
+print(f"Gene {gene_name}: {client}")
+
+for i in range (0, 5):
+    gene, response = s.debug_talk(useful_gene[10*i:10 + 10*i])
+    print(f"Fragment {i + 1}: {gene}")
