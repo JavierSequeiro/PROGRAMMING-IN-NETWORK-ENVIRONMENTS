bc2596bf2fed18c212f8184166da62e6d3f7ff08
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-25 08:01:59 +0100

    PRACTICE 2: EXERCISE 5 SEND GENE TO SERVER

--- P2/Ex5.py
+++ P2/Ex5.py
@@ -1,9 +1,16 @@
 from Client0 import Client
-from pathlib import Path
+import colorama
+from Seq1 import Seq
 
-print()
-Client.ping()
+print("------| Practice 2, Exercise 5 |------")
 s = Client("192.168.1.38", 8080)
-gene = Path("../Session-04/U5.txt").read_text()
-print(s.talk("Sending U5 gene to the server..."))
-print(s.talk(gene))
+gene_name = "U5"
+gene_file = f"../Session-04/{gene_name}.txt"
+useful_gene = Seq().read_fasta(gene_file)
+colorama.init(autoreset=True)
+client_info, server_info = s.debug_talk(f"Sending {gene_name} gene to the server...")
+print(client_info)
+print(server_info)
+client, server = s.debug_talk(useful_gene)
+print(client)
+print(server)
