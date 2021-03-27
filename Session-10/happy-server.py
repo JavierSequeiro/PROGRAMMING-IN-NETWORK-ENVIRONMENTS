6eeea8046673e27eef713e3e9e6e755397cb26ef
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-03-27 16:52:06 +0100

    HAPPY SERVER FINISHED (EXCEPT KEYBOARD INTERRUPT NOT WORKING)

--- Session-10/happy-server.py
+++ Session-10/happy-server.py
@@ -1,8 +1,11 @@
 import socket
+import colorama
+from colorama import Fore
 
 PORT = 8080
 IP = "192.168.1.38"
 
+colorama.init(autoreset=True)
 sl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sl.bind((IP, PORT))
 sl.listen()
@@ -20,8 +23,8 @@ while True:
         print("A client has connected to the server! ")
         msg_bytes = client_socket.recv(2048)
         msg_string = msg_bytes.decode()
-        print(f"Message received: {msg_string}")
+        print(f"Message received: {Fore.GREEN + msg_string}")
 
-        server_response = "Welcome to the jungle"
+        server_response = f"ECHO: {msg_string}"
         client_socket.send(server_response.encode())
         client_socket.close()
