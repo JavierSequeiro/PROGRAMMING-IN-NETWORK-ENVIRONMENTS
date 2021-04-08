159dcbe2a5e53de114385d30c7f6ed2b387ed2a5
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-04-08 10:16:34 +0200

    ADDED GET FUNCTION EXERCISE 2

--- P3/server_utils.py
+++ P3/server_utils.py
@@ -1,3 +1,19 @@
 import colorama
 from colorama import Fore
-from Seq1 import Seq
+#from Seq1 import Seq
+
+colorama.init(autoreset=True)
+def modified_message(message):
+    return message.replace("\n", "").replace("\r", "")
+
+def ping(client_socket):
+    print(Fore.GREEN + "PING command")
+    server_response = "OK!\n"
+    print(server_response)
+    client_socket.send(server_response.encode())
+
+def get_sequence(client_socket, index, gene_list):
+    sequence = gene_list[index]
+    print(Fore.GREEN + "GET")
+    print(sequence)
+    client_socket.send(sequence.encode())
