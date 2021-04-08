3338de3fb9ff63f7f671280fb029a6c01998e7c1
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-04-08 11:02:59 +0200

    ADDED INFO FUNCTION EXERCISE 3

--- P3/server_utils.py
+++ P3/server_utils.py
@@ -1,6 +1,6 @@
 import colorama
 from colorama import Fore
-#from Seq1 import Seq
+from Seq1 import Seq
 
 colorama.init(autoreset=True)
 def modified_message(message):
@@ -17,3 +17,24 @@ def get_sequence(client_socket, index, gene_list):
     print(Fore.GREEN + "GET")
     print(sequence)
     client_socket.send(sequence.encode())
+
+def info(client_socket, sequence):
+    print(Fore.GREEN + "INFO")
+
+    useful_seq = Seq(sequence)
+    A, C, T, G = useful_seq.count_base()
+
+    seq_info = f"Sequence: {sequence}"
+    print(seq_info)
+    client_socket.send(seq_info.encode())
+
+    seq_len = f"Total length: {len(sequence)}"
+    print(seq_len)
+    client_socket.send(seq_len.encode())
+
+    nucleotides_list = [A, C, T, G]
+    nucleotides_names = ["A", "C", "T", "G"]
+    for i in range(0, 4):
+        nuc_info = f"{nucleotides_names[i]}: {nucleotides_list[i]} ({(nucleotides_list[i] * 100) / len(sequence)}%)"
+        print(nuc_info)
+        client_socket.send(nuc_info.encode())
