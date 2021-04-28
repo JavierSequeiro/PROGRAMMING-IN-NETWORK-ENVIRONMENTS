303adb9e1590e27fd60ddb7bfb06ed5fbdb2e7d5
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-04-28 17:41:21 +0200

    SEQ SERVER

---
+++ P6/Seq-Server.py
@@ -0,0 +1,47 @@
+import socket
+import colorama
+import server_utils
+
+PORT = 8080
+IP = "127.0.0.1"
+seq_list = ["ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA", "AAAAACATTAATCTGTGGCCTTTCTTTGCCATTTCCAACTCTGCCACCTCCATCGAACGA", "CAAGGTCCCCTTCTTCCTTTCCATTCCCGTCAGCTTCATTTCCCTAATCTCCGTACAAAT", "CCCTAGCCTGACTCCCTTTCCTTTCCATCCTCACCAGACGCCCGCATGCCGGACCTCAAA", "AGCGCAAACGCTAAAAACCGGTTGAGTTGACGCACGGAGAGAAGGGGTGTGTGGGTGGGT"]
+
+colorama.init(autoreset=True)
+sl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sl.bind((IP, PORT))
+sl.listen()
+print("SEQ Server is configured!")
+while True:
+    print("Waiting for clients!")
+    try:
+        (client_socket, client_ip_port) = sl.accept()
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+        sl.close()
+        exit()
+
+    else:
+        msg_bytes = client_socket.recv(2048)
+        msg_string = msg_bytes.decode()
+        useful_string = server_utils.modified_message(msg_string)
+        separate_strings = useful_string.split(" ")
+
+        if "PING" == server_utils.modified_message(msg_string):
+            server_utils.ping(client_socket)
+
+        elif separate_strings[0] == "GET":
+            server_utils.get_sequence(client_socket, int(separate_strings[1]), seq_list)
+
+        elif separate_strings[0] == "INFO":
+            server_utils.info(client_socket, separate_strings[1])
+
+        elif separate_strings[0] == "COMP":
+            server_utils.complementary(client_socket, separate_strings[1])
+
+        elif separate_strings[0] == "REV":
+            server_utils.reverse(client_socket, separate_strings[1])
+
+        elif separate_strings[0] == "GENE":
+            server_utils.gene(client_socket, separate_strings[1])
+        client_socket.close()
+
