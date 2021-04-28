a8f890230501fc85151d4d4ab10a3839800b05c0
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-04-28 19:00:54 +0200

    SEQ FUNCTIONS MODIFIED

--- P6/server_utils.py
+++ P6/server_utils.py
@@ -1,6 +1,8 @@
 import colorama
 from colorama import Fore
 from Seq1 import Seq
+from pathlib import Path
+import jinja2
 
 colorama.init(autoreset=True)
 def modified_message(message):
@@ -12,11 +14,17 @@ def ping(client_socket):
     print(server_response)
     client_socket.send(server_response.encode())
 
-def get_sequence(client_socket, index, gene_list):
-    sequence = gene_list[index]
-    print(Fore.GREEN + "GET")
-    print(sequence)
-    client_socket.send(sequence.encode())
+def get_sequence(seq_number, gene_list):
+    sequence = gene_list[int(seq_number)]
+    context = {"number": seq_number,
+               "sequence": sequence}
+    contents = read_template_html_file("./HTML_FILES/get.html").render(context=context)
+    return contents
+
+def read_template_html_file(filename):
+    contents = jinja2.Template(Path(filename).read_text())
+    return contents
+
 
 def info(client_socket, sequence):
     print(Fore.GREEN + "INFO")
@@ -54,14 +62,12 @@ def reverse(client_socket, sequence):
     print(reverse_seq)
     client_socket.send(reverse_seq.encode())
 
-def gene(client_socket, gene):
-    print(Fore.GREEN + "GENE")
+def gene(gene):
     sequence = Seq()
-    try:
-        complete_seq = sequence.read_fasta(f"{gene}.txt")
-        print(complete_seq)
-        client_socket.send(complete_seq.encode())
-    except FileNotFoundError:
-        print("THE CLIENT MUST ENTER AN AVAILABLE GENE")
-        not_available_message = "YOU MUST ENTER AN AVAILABLE GENE\n [U5, FRAT1, FXN, RNU6_269P]"
-        client_socket.send(not_available_message.encode())
+    complete_seq = sequence.read_fasta(f"./Sequences/{gene}.txt")
+    context = {
+        "gene_name": gene,
+        "gene_contents": sequence.strbases
+    }
+    contents = read_template_html_file("./HTML_FILES/gene.html").render(context=context)
+    return contents
