0a66d84e8faab81b1da37221fb8a503169f174df
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-05-06 00:20:38 +0200

    EXERCISE 5: PRINT DATA FROM ALL GENES IN DATA BASE

---
+++ P7/Ex05-gene-all.py
@@ -0,0 +1,41 @@
+import http.client
+import json
+from Seq1 import Seq
+
+gene_dict = {
+    "FRAT1": "ENSG00000165879",
+    "ADA": "ENSG00000196839",
+    "FXN": "ENSG00000165060",
+    "RNU6_269P": "ENSG00000212379",
+    "MIR633": "ENSG00000207552",
+    "TTTY4C": "ENSG00000228296",
+    "RBMY2YP": "ENSG00000227633",
+    "FGFR3": "ENSG00000068078",
+    "KDR": "ENSG00000128052",
+    "ANK2": "ENSG00000145362"
+}
+SERVER = "rest.ensembl.org"
+ENDPOINT = "/sequence/id/"
+PARAMETERS = "?content-type=application/json"
+
+connection = http.client.HTTPConnection(SERVER)
+
+try:
+    for gene in gene_dict.keys():
+        id = gene_dict[gene]
+        connection.request("GET", ENDPOINT + id + PARAMETERS)
+        response = connection.getresponse()
+        print(f"SERVER: {SERVER}")
+        print(f"URL: {SERVER + ENDPOINT + PARAMETERS}")
+        print(f"Response received!: {response.status} {response.reason} \n")
+        if response.status == 200:
+            response = json.loads(response.read().decode())
+            print(f"Gene: {gene}")
+            print("Description:", response["desc"])
+            sequence = Seq(response["seq"])
+            print(sequence.info())
+            print(f"Most frequent base: {sequence.most_frequent_base()} \n")
+
+except KeyError:
+    print("The gene is not inside the data base. Choose between the following:", list(gene_dict.keys()))
+
