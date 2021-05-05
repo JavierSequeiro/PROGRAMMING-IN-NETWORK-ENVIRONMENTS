b522fab9d3cbffe2ba23ba3cb72193d351d6ede3
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-05-05 18:18:52 +0200

    EXERCISE 3

---
+++ P7/EX03-gene-MIR633.py
@@ -0,0 +1,30 @@
+import http.client
+import json
+gene_dict = {
+    "FRAT1": "ENSG00000165879",
+    "ADA": "ENSG00000196839",
+    "FXN": "ENSG00000165060",
+    "RNU6_269P": "ENSG00000212379",
+    "MIR633": "ENSG00000207552",
+    "TTTY4C": "ENSG00000228296",
+    "RBMY2YP": "ENSG00000227633",
+    "FGFR3": "ENSG00000068078",
+    "KDR": "ENSG00000128052",
+    "ANK2": "ENSG00000145362"
+}
+SERVER = "resembl.org"
+ENDPOINT = "/sequence/id"
+ID = gene_dict["MIR633"]
+PARAMETERS = "?content-type=application/json"
+print(f"SERVER: {SERVER}")
+print(f"URL {SERVER + ENDPOINT + PARAMETERS}")
+
+connection = http.client.HTTPConnection(SERVER)
+connection.request("GET", ENDPOINT + ID + PARAMETERS)
+response = connection.getresponse()
+response = json.loads(response.read().decode())
+#dict_response = json.loads(response)
+#print(json.dumps(response, indent=4, sort_keys=True))
+print(f"Gene: {ID}")
+print("Description:", response["desc"])
+print("Bases: ", response["seq"])
