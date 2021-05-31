bf48ccf4124372c7e8c06cf037e5ce18c9154e8e
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-05-31 11:45:58 +0200

    ADDED JSON.LOADS TO GET THE DICT FORMAT OF THE RESPONSE

--- Final-Project/client.py
+++ Final-Project/client.py
@@ -1,6 +1,6 @@
 import http.client
 import json
-
+from pathlib import Path
 PORT = 8080
 SERVER = 'localhost'
 
@@ -11,36 +11,62 @@ conn = http.client.HTTPConnection(SERVER, PORT)
 
 # -- Send the request message, using the GET method. We are
 # -- requesting the main page (/)
-try:
-    #TESTS FOR 1
-    #conn.request("GET", "/listSpecies?limit=&json=1")
-    #conn.request("GET", "/listSpecies?limit=10&json=1")
-    #TESTS FOR 2
-    #conn.request("GET", "/karyotype?specie=human&json=1")
-    #conn.request("GET", "/karyotype?specie=FER&json=1")
-    #TESTS FOR 3
-    #conn.request("GET", "/chromosomeLength?specie=human&chromo=21&json=1")
-    #conn.request("GET", "/chromosomeLength?specie=human&chromo=34&json=1")
-    #
-    #TESTS FOR 4
-    #conn.request("GET", "/geneSeq?gene=ADA&json=1")
-    #conn.request("GET", "/geneSeq?gene=WHATEVER&json=1")
-    #TESTS FOR 5
-    #conn.request("GET", "/geneInfo?gene=FRAT1&json=1")
-    #conn.request("GET", "/geneInfo?gene=WHATEVER&json=1")
-    #TESTS FOR 6
-    conn.request("GET", "/geneCalc?gene=FRAT1&json=1")
-    #conn.request("GET", "/geneCalc?gene=WHATEVER&json=1")
+URL_TEST_LIST = ["/listSpecies?limit=&json=1", "/listSpecies?limit=10&json=1", "/karyotype?specie=human&json=1", "/karyotype?specie=FER&json=1",
+                 "/chromosomeLength?specie=human&chromo=21&json=1", "/chromosomeLength?specie=human&chromo=34&json=1",
+                 "/chromosomeLength?specie=whatever&chromo=34&json=1", "/geneSeq?gene=ADA&json=1", "/geneSeq?gene=WHATEVER&json=1",
+                 "/geneInfo?gene=FRAT1&json=1", "/geneInfo?gene=WHATEVER&json=1", "/geneCalc?gene=FRAT1&json=1", "/geneCalc?gene=WHATEVER&json=1"]
+for url in URL_TEST_LIST:
+    try:
+        conn.request("GET", url)
+        #TESTS FOR 1
+        #conn.request("GET", "/listSpecies?limit=&json=1")
+        #conn.request("GET", "/listSpecies?limit=10&json=1")
+        #TESTS FOR 2
+        #conn.request("GET", "/karyotype?specie=human&json=1")
+        #conn.request("GET", "/karyotype?specie=FER&json=1")
+        #TESTS FOR 3
+        #conn.request("GET", "/chromosomeLength?specie=human&chromo=21&json=1")
+        #conn.request("GET", "/chromosomeLength?specie=human&chromo=34&json=1")
+        #conn.request("GET", "/chromosomeLength?specie=whatever&chromo=34&json=1")
+        #TESTS FOR 4
+        #conn.request("GET", "/geneSeq?gene=ADA&json=1")
+        #conn.request("GET", "/geneSeq?gene=WHATEVER&json=1")
+        #TESTS FOR 5
+        #conn.request("GET", "/geneInfo?gene=FRAT1&json=1")
+        #conn.request("GET", "/geneInfo?gene=WHATEVER&json=1")
+        #TESTS FOR 6
+        #conn.request("GET", "/geneCalc?gene=FRAT1&json=1")
+        #conn.request("GET", "/geneCalc?gene=WHATEVER&json=1")
 
-except ConnectionRefusedError:
-    print("ERROR! Cannot connect to the Server")
-    exit()
-r1 = conn.getresponse()
-print(f"Response received!: {r1.status} {r1.reason}\n")
+    except ConnectionRefusedError:
+        print("ERROR! Cannot connect to the Server")
+        exit()
+    r1 = conn.getresponse()
+    print(f"Response received!: {r1.status} {r1.reason}\n")
 
-# -- Read the response's body
-data1 = r1.read().decode("utf-8")
-json_data = json.dumps(data1)
+    # -- Read the response's body
+    data1 = r1.read().decode("utf-8")
+    json_data_string = json.dumps(data1)
+    json_data_dict = json.loads(json_data_string)
+    # -- Print the received data
+    print(f"CONTENT: {json_data_dict}")
 
-# -- Print the received data
-print(f"CONTENT: {json_data}")
+
+
+    file = Path("report-advanced.txt").open("a")
+    my_endpoint = url.strip("/").split("?")[0]
+    path_name = url.split("?")[0]
+    if path_name != "/favicon.ico" and path_name != "/":
+        # my_endpoint = path_name.strip("/")
+        file.write(f"----> {my_endpoint} endpoint \n")
+        # CHANGE NUMBER OF TEST MANUALLY
+        file.write(f"TEST \n\n")
+        file.write(f"* Input: http://127.0.0.1:8080{url} \n"
+                   f"\n"
+                   f"* Output: \n\n")
+        file.write(json_data_dict)
+        file.write("\n\n"
+                   "==================\n")
+    else:
+        pass
+    file.close()
