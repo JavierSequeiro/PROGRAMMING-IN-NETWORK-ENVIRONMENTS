fe97d2f28c3e424472a10f341b2246f5fad5efbe
JavierSequeiro <j.sequeiro.2019@alumnos.urjc.es>
2021-06-02 10:33:25 +0200

    P3 CLIENT FOR TESTING

--- P3/test_client.py
+++ P3/test_client.py
@@ -12,35 +12,38 @@ print(client)
 
 #PING TESTING
 print("* Testing PING...")
-print(client.talk("PING"))
+try:
+    print(client.talk("PING"))
 
-#GET TESTING
-print("* Testing GET...")
-for i in range(0, 5):
-    sequence = client.talk(f"GET {i}")
-    print(f"GET {i}: {sequence}")
+    #GET TESTING
+    print("* Testing GET...")
+    for i in range(0, 5):
+        sequence = client.talk(f"GET {i}")
+        print(f"GET {i}: {sequence}")
 
-#INFO TESTING
-print("* Testing INFO...")
-print(client.talk(f"INFO {seq_list[0]}"))
+    #INFO TESTING
+    print("* Testing INFO...")
+    print(client.talk(f"INFO {seq_list[0]}"))
 
-#COMP TESTING
-print("* Testing COMP...")
-print(f"COMP {seq_list[0]}")
-print(client.talk(f"COMP {seq_list[0]}"))
+    #COMP TESTING
+    print("* Testing COMP...")
+    print(f"COMP {seq_list[0]}")
+    print(client.talk(f"COMP {seq_list[0]}"))
 
-#REV TESTING
-print("Testing REV...")
-print(f"REV {seq_list[0]}")
-print(client.talk(f"REV {seq_list[0]}"))
+    #REV TESTING
+    print("Testing REV...")
+    print(f"REV {seq_list[0]}")
+    print(client.talk(f"REV {seq_list[0]}"))
 
-#GENE TESTING
-gene_list = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
-print("* Testing GENE...")
-for gene in gene_list:
-    print(f"GENE {gene}")
-    print(client.talk(f"GENE {gene}\n"))
+    #GENE TESTING
+    gene_list = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
+    print("* Testing GENE...")
+    for gene in gene_list:
+        print(f"GENE {gene}")
+        print(client.talk(f"GENE {gene}\n"))
 
+except ConnectionRefusedError:
+    print("THE SERVER IS NOT AVAILABLE")
 
 
 
